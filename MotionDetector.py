3115ff0aa5971497716afbe1b67f4e78f3cdb228
Edgar Han <edgarh@vt.edu>
2019-10-07 21:40:40 -0400

    added Motion detection

--- MotionDetector.py
+++ MotionDetector.py
@@ -1,80 +1,96 @@
-import cv2
+import cv2, time
+import numpy as np
+from datetime import datetime
 
 class MotionDetectorContour:
-    def __init__(self,ceil=15):
-        self.ceil = ceil
-        self.url = "http://192.168.50.238:8080/video"
-        self.capture = cv2.CaptureFromCAM(url)
-        cv2.NamedWindow("Target", 1)
+    def __init__(self, src):
+        self.src            = src
+        self.window_name    = "Window - {}".format(src)
+        self.cap            = cv2.VideoCapture(self.src)
+        self.video_out      = None
+        self.prev_frame     = None
+        self.moving_avg     = None
+        self.duration       = 0
 
     def run(self):
-        # Capture first frame to get size
-        frame = cv2.QueryFrame(self.capture)
-        frame_size = cv2.GetSize(frame)
-
-        width = frame.width
-        height = frame.height
-        surface = width * height #Surface area of the image
-        cursurface = 0 #Hold the current surface that have changed
-
-        grey_image = cv2.CreateImage(cv2.GetSize(frame), cv2.IPL_DEPTH_8U, 1)
-        moving_average = cv2.CreateImage(cv2.GetSize(frame), cv2.IPL_DEPTH_32F, 3)
-        difference = None
-
         while True:
-            color_image = cv2.QueryFrame(self.capture)
+            # Grab Frame from capture source
+            check, frame = self.cap.read()
 
-            cv2.Smooth(color_image, color_image, cv2.CV_GAUSSIAN, 3, 0) #Remove false positives
+            #print(check)
 
-            if not difference: #For the first time put values in difference, temp and moving_average
-                difference = cv2.CloneImage(color_image)
-                temp = cv2.CloneImage(color_image)
-                cv2.ConvertScale(color_image, moving_average, 1.0, 0.0)
-            else:
-                cv2.RunningAvg(color_image, moving_average, 0.020, None) #Compute the average
+            # Process Frame
+            #frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
+            proc_frame = self.process_frame(frame)
+            if proc_frame is None:
+                continue
 
-            # Convert the scale of the moving average.
-            cv2.ConvertScale(moving_average, temp, 1.0, 0.0)
+            # Check for 'q' key press to quit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+               break
 
-            # Minus the current frame from the moving average.
-            cv2.AbsDiff(color_image, temp, difference)
+    def process_frame(self, frame):
+        # Change to gray scale and remove minor
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        if self.prev_frame is None:
+            self.prev_frame = gray
+            self.moving_avg = np.float32(gray)
+            return None
 
-            #Convert the image so that it can be thresholded
-            cv2.CvtColor(difference, grey_image, cv2.CV_RGB2GRAY)
-            cv2.Threshold(grey_image, grey_image, 70, 255, cv2.CV_THRESH_BINARY)
 
-            cv2.Dilate(grey_image, grey_image, None, 18) #to get object blobs
-            cv2.Erode(grey_image, grey_image, None, 10)
+        # Get diff of previous frame and current frame
+        #print(gray.shape, self.prev_frame.shape)
+        diff_frame = cv2.absdiff(self.prev_frame, gray)
 
-            # Find contours
-            storage = cv2.CreateMemStorage(0)
-            contours = cv2.FindContours(grey_image, storage, cv2.CV_RETR_EXTERNAL, cv2.CV_CHAIN_APPROX_SIMPLE)
+        # Change static areas to white for thresholds of 25
+        thresh_frame = cv2.threshold(diff_frame, 25, 255, cv2.THRESH_BINARY)[1]
+        thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
+        # Finding contour and hierarchy from a moving object.
+        contours, hierachy = cv2.findContours(thresh_frame.copy(),
+            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            if cv2.contourArea(contour) < 10000:
+                continue
+            #motion = 1
+            self.init_record('video-{}.mp4'.format(time.time()), frame, 50)
+            (x, y, w, h) = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-            backcontours = contours #Save contours
+        # Find average
+        cv2.accumulateWeighted(gray, self.moving_avg, 0.05)
+        self.prev_frame = cv2.convertScaleAbs(self.moving_avg)
 
-            while contours: #For all contours compute the area
-                cursurface += cv2.ContourArea(contours)
-                contours = contours.h_next()
+        # Show Frames, this needs to move out
+        self.show_frame(gray, "Gray Frame")
+        self.show_frame(thresh_frame, "Threshold Frame")
+        self.show_frame(frame, "Original Frame")
+        self.show_frame(self.prev_frame, "previous Frame")
 
-            avg = (cursurface*100)/surface #Calculate the average of contour area on the total size
-            if avg > self.ceil:
-                print ("Something is moving !")
-            #print avg,"%"
-            cursurface = 0 #Put back the current surface to 0
+        self.frame_record(frame)
 
-            #Draw the contours on the image
-            _red =  (0, 0, 255); #Red for external contours
-            _green =  (0, 255, 0);# Gren internal contours
-            levels=1 #1 contours drawn, 2 internal contours as well, 3 ...
-            cv2.DrawContours (color_image, backcontours,  _red, _green, levels, 2, cv2.CV_FILLED)
+        return frame 
 
-            cv2.ShowImage("Target", color_image)
+    def init_record(self, filename, frame, duration):
+        if self.duration > 0:
+            return
+        width = frame.shape[1]
+        height = frame.shape[0]
+        self.duration = duration
+        self.video_out = cv2.VideoWriter(filename, CV_FOURCC('M','P','G','4'), 25, (width, height))
 
-            # Listen for ESC or ENTER key
-            c = cv2.WaitKey(7) % 0x100
-            if c == 27 or c == 10:
-                break
+    def frame_record(self, frame):
+        if self.duration < 1:
+            return
+        self.video_out.write(frame)
+        if self.duration == 1:
+            self.video_out.write(frame)
+        self.duration -= 1
+
+    def show_frame(self, frame, win_title):
+        cv2.imshow("{} - {}".format(self.window_name, win_title), frame)
 
 if __name__=="__main__":
-    t = MotionDetectorContour()
+    url = "http://192.168.50.249:8080/video"
+    t = MotionDetectorContour(url)
     t.run()
